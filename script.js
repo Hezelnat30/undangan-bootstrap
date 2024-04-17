f1c0c85043afe98eb5200493d483aea9eb9819cc
Hezelnat30 <justin.hezekiel123@gmail.com>
2024-04-17 23:36:37 +0700

    commit pertama

---
+++ script.js
@@ -0,0 +1,45 @@
+const rootElement = document.querySelector(":root");
+const audioIconWrapper = document.querySelector('.audio-icon-wrapper');
+const song = document.querySelector('#song');
+const audioIcon = document.querySelector('.audio-icon-wrapper i');
+let isPlaying = false;
+function disableScroll() {
+    scrollTop = window.scrollY || document.documentElement.scrollTop;
+    scrollLeft = window.scrollX || document.documentElement.scrollLeft;
+
+    window.onscroll = function () {
+        window.scrollTo(scrollTop, scrollLeft);
+    }
+
+    rootElement.style.scrollBehavior = 'auto';
+}
+function enableScroll() {
+    window.onscroll = function () {
+    rootElement.style.scrollBehavior = 'smooth'
+
+    playAudio();
+    }
+}
+
+function playAudio() {
+    song.volume = 0.5;
+    audioIconWrapper.style.display = 'flex'
+    song.play()
+    isPlaying = true;
+}
+
+audioIconWrapper.onclick = function () {
+    if(isPlaying) {
+        song.pause();
+        audioIcon.classList.remove('bi-disc')
+        audioIcon.classList.add('bi-pause-circle')
+    } else {
+        song.play();
+        audioIcon.classList.add('bi-disc')
+        audioIcon.classList.remove('bi-pause-circle')
+    }
+
+    isPlaying = !isPlaying;
+}
+
+disableScroll()
